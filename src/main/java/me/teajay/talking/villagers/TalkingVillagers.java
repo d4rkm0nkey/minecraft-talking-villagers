2d9ce0af73442dc0e4a9624f58568fed067f51fe
d4rkm0nkey <Tim.Jeske@web.de>
2021-03-31 01:01:33 +0200

    Remove unused code

--- src/main/java/me/teajay/talking/villagers/TalkingVillagers.java
+++ src/main/java/me/teajay/talking/villagers/TalkingVillagers.java
@@ -1,14 +1,12 @@
 package me.teajay.talking.villagers;
 
 import me.teajay.talking.villagers.common.sound.VillagerVoiceManager;
-import me.teajay.talking.villagers.common.data.ResourceLoader;
 import net.fabricmc.api.ModInitializer;
 
 public class TalkingVillagers implements ModInitializer {
 	public static final String MODID = "talkingvillagers";
 	@Override
 	public void onInitialize() {
-		ResourceLoader.initLoader();
 		VillagerVoiceManager.initialiseVoices();
 	}
 }
